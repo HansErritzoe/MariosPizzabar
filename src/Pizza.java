561974532eb6e79e12a12a5b532dd1e321eae4e3
Lina <lial0001@stud.kea.dk>
2024-04-08 16:34:49 +0200

    tilføjet pizza class og pizzaListe.txt

--- src/Pizza.java
+++ src/Pizza.java
@@ -3,4 +3,14 @@ public class Pizza {
     private String name;
     private double price;
 
+    public pizza (String name, double price){
+        this.name=name;
+        this.price=price;
+    }
+
+    public String toString(){
+        return (name+ ": " +price);
+    }
+
+
 }
