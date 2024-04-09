17c08c80e6304ed6948dfcb1912e3abab0027e37
HansErritzoe <h.erritzoe@hotmail.com>
2024-04-09 14:15:05 +0200

    Tilføjet hentOrdrer() i FilBehandlings klasse samt arrays i Main til opbevaring af Ordrer og færdigeOrdrer

--- src/FilBehandling.java
+++ src/FilBehandling.java
@@ -5,9 +5,9 @@ public class FilBehandling {
 
     //metode til at gemme alle ordre i et array til ordre.txt filen
     public static void gemOrdre(Ordre[] ordreArray){
-        String filepath = "ordre.txt";
+        String filNavn = "ordrer.txt";
         try {
-            PrintStream output = new PrintStream(filepath);
+            PrintStream output = new PrintStream(filNavn);
             for(int i = 0; i < ordreArray.length; i++) {
                 output.print(ordreArray[i].toString()+"\n");
             }
@@ -15,6 +15,38 @@ public class FilBehandling {
             throw new RuntimeException(e);
         }
     }
+
+    // (Ikke færdig) metode til at returnere ordrer fra ordrer.txt som Ordre[]
+    public static Ordre[] hentOrdrer(){
+        String filNavn = "ordrer.txt";
+        try {
+            Ordre[] nytArray = new Ordre[tælLinjerIFil(filNavn)];
+            Scanner linjeLæser = new Scanner(filNavn);
+            for(int i = 0; i < nytArray.length;i++){
+
+            }
+        } catch (FileNotFoundException e){
+            throw new RuntimeException();
+        }
+    }
+
+    //tæller hvor mange linjer er i en fil, så vi ved hvor stort et array vi skal lave når vi henter fra fil
+    public static int tælLinjerIFil(String filNavn) throws FileNotFoundException {
+        File fil = new File(filNavn);
+        Scanner scanner = new Scanner(fil);
+        int linjer = 0;
+        try {
+            while (scanner.hasNextLine()) {
+                scanner.nextLine();
+                linjer++;
+            }
+        } finally {
+            scanner.close();
+        }
+        return linjer;
+    }
+
+
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import java.io.*;
 import java.util.*;
 public class Main {
 
+    //henter nuværende ordrer og færdige ordrer når programmet starter op fra filer
+    public static Ordre[] ordrer = FilBehandling.hentOrdrer();
+    //public static Ordre[] færdigeOrdrer = FilBehandling.hentFærdigeOrdrer(); //to be done
+
     //scanner til genbrug i hele koden
     public static final Scanner brugerInput = new Scanner(System.in);
 
