7f34ddca46361e886124661caf79316391c3fbff
HansErritzoe <h.erritzoe@hotmail.com>
2024-04-09 11:58:47 +0200

    Tilføjet FilBehandling klasse samt gemOrdre() metode

---
+++ src/FilBehandling.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class FilBehandling {
+
+    //metode til at gemme alle ordre i et array til ordre.txt filen
+    public static void gemOrdre(Ordre[] ordreArray){
+        String filepath = "ordre.txt";
+        try {
+            PrintStream output = new PrintStream(filepath);
+            for(int i = 0; i < ordreArray.length; i++) {
+                output.print(ordreArray[i].toString()+"\n");
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    }
+
+}
